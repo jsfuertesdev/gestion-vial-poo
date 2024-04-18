34a5735271a0afa51bf9e1bb6be03bd6e4346ad2
jsfuertesdev <jsfuertesdev@gmail.com>
2024-04-18 01:13:12 -0500

    Add Navbar

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+function Navbar() {
+ return (
+    <nav className="bg-blue-500 p-4 flex justify-between items-center">
+      <div className="text-white font-bold text-xl">Gestión Vial</div>
+      <div className="flex space-x-4">
+        <Link href="/" className="text-white hover:text-blue-300">Dashboard</Link>
+        <Link href="/comparendos" className="text-white hover:text-blue-300">Comparendos</Link>
+      </div>
+    </nav>
+ );
+}
+
+export default Navbar;
